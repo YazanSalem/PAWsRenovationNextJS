050b54ff07333d33f3a6fe77ad4a54e10d52fcd2
YazanSalem <yazansalem064@gmail.com>
2023-04-06 01:15:17 -0500

    homepage

--- paws-renovation/app/components/footer.tsx
+++ paws-renovation/app/components/footer.tsx
@@ -1,13 +1,13 @@
 export default function Footer(){
     return (
-        <footer className="px-4 py-4 h-auto mb-4 w3-full flex flex-wrap justify-center items-center bg-gray-800 text-white">
+        <footer className="px-4 py-4 h-auto mb-4 w3-full flex flex-wrap justify-center items-center bg-yellow-500 text-white">
             <div className="flex flex-wrap items-center">
                 <img className="h-6 w-6 rounded-full bg-grey-700"
                  alt="Use any sample image here..." 
                  src="https://i0.wp.com/uwmpost.com/wp-content/uploads/2016/11/paws.1.u.png?ssl=1"/>
                 <span className="font-semibold text-sm ml-2">    </span>
-                <span className="text-sm text-gray-500">&#160;|&#160;All rights r</span>
-                <span className="text-sm text-gray-500">eserved</span>
+                <span className="text-sm text-white-500">&#160;|&#160;All rights r</span>
+                <span className="text-sm text-white-500">eserved</span>
             </div>
         </footer>
     )
