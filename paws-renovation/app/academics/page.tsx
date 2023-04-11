52ddda224b4326bbc23d9c909a0bbcd828781b89
boettc74 <boettc74@uwm.edu>
2023-04-11 18:34:27 -0500

    Added Shopping Cart

--- paws-renovation/app/academics/page.tsx
+++ paws-renovation/app/academics/page.tsx
@@ -5,9 +5,53 @@ export default function Academics(){
     return(
         <>
         <Navbar/>
-        <div className="overflow-hidden bg-white min-h-screen shadow sm:rounded-lg">
-            <div className="px-4 py-5 sm:px-6">
-                <h3 className="text-base font-semibold leading-6 text-gray-900">Academics Page</h3>
+        <div className=" px-4 overflow-hidden bg-white min-h-screen shadow sm:rounded-lg">
+            <div className="px-3 py-5 sm:px-6">
+                <h3 className="text-base font-semibold leading-6 text-gray-900">Active Classes</h3>
+            </div>
+            <div className="border-2 border-gray-200">
+                    <div className="border-b-4 border-thick border-black-100">
+                        <div className="bg-gray-50 px-12 py-0 sm:grid sm:grid-cols-5 sm:gap-1 sm:px-1">
+                            <dt className="text-medium font-medium text-black-500">Course</dt>
+                            
+                            <dt className="text-medium font-medium text-black-500">Term</dt>
+                            
+                            <dt className="text-medium font-medium text-black-500">Professor</dt>
+                            
+                            <dt className="text-medium font-medium text-black-500">Status</dt>
+                            
+                            <dt className="text-medium font-medium text-black-500">Grade</dt>
+                        </div>
+                    </div>
+                    <div>
+                        <div className="bg-gray-50 px-12 py-2 sm:grid sm:grid-cols-5 sm:gap-1 sm:px-1">
+                            <dt className="text-small font-small text-black-500">Classname</dt>
+                            
+                            <dt className="text-small font-small text-black-500">Term</dt>
+                            
+                            <dt className="text-small font-small text-black-500">Professor</dt>
+                            
+                            <dt className="text-medium font-small text-black-500">Enrolled</dt>
+                            
+                            <dt className="text-medium font-medium text-black-500">A</dt>
+                        </div>
+                    </div>
+                    <div>
+                        <div className="bg-gray-50 px-12 py-2 sm:grid sm:grid-cols-5 sm:gap-1 sm:px-1">
+                            <dt className="text-small font-small text-black-500">Classname</dt>
+                            
+                            <dt className="text-small font-small text-black-500">Term</dt>
+                            
+                            <dt className="text-small font-small text-black-500">Professor</dt>
+                            
+                            <dt className="text-medium font-small text-black-500">Enrolled</dt>
+                            
+                            <dt className="text-medium font-medium text-black-500">A</dt>
+                        </div>
+                    </div>         
+            </div>
+            <div className="px-3 py-5 sm:px-6">
+                <h3 className="text-base font-semibold leading-6 text-gray-900">Shopping Cart</h3>
             </div>
             <div className="border-2 border-gray-200">
                     <div className="border-b-4 border-thick border-black-100">
@@ -18,7 +62,7 @@ export default function Academics(){
                             
                             <dt className="text-medium font-medium text-black-500">Professor</dt>
                             
-                            <dt className="text-medium font-medium text-black-500">Status</dt>
+                            <dt className="text-medium font-medium text-black-500">Availabilty</dt>
                         </div>
                     </div>
                     <div>
@@ -29,7 +73,7 @@ export default function Academics(){
                             
                             <dt className="text-small font-small text-black-500">Professor</dt>
                             
-                            <dt className="text-medium font-small text-black-500">Enrolled</dt>
+                            <dt className="text-medium font-small text-black-500">Waitlist</dt>
                         </div>
                     </div>
                     <div>
@@ -40,7 +84,7 @@ export default function Academics(){
                             
                             <dt className="text-small font-small text-black-500">Professor</dt>
                             
-                            <dt className="text-medium font-small text-black-500">Enrolled</dt>
+                            <dt className="text-medium font-small text-black-500">Waitlist</dt>
                         </div>
                     </div>         
             </div>
@@ -55,6 +99,7 @@ export default function Academics(){
                 <dt></dt>
             </div>           
         </div>
+        
     <Footer/>
     </>
     )
