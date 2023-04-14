37d7016f316a87bda446eface4a59e96b2a31d72
YazanSalem <yazansalem064@gmail.com>
2023-04-14 00:27:56 -0500

    small fix

--- paws-renovation/app/loginComponents/LoginPageForm.tsx
+++ paws-renovation/app/loginComponents/LoginPageForm.tsx
@@ -41,7 +41,7 @@ export default function LoginPageForm({inputs, handleChangeInput, handleClick} :
               <input
                 id="password"
                 name="password"
-                type="text"
+                type="password"
                 autoComplete="current-password"
                 required
                 className="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-yellow-600 sm:text-sm sm:leading-6"
